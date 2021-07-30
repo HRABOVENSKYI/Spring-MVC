77cdbb32303223260555072b3a77514de84f94ef
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-07-30 14:56:14 +0300

    Added InternalResourceViewResolver to set viwes configs (view directory, prefix and suffix)

--- src/main/java/org/example/config/SpringConfig.java
+++ src/main/java/org/example/config/SpringConfig.java
@@ -1,9 +1,19 @@
 package org.example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan("org.example")
 public class SpringConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver vr = new InternalResourceViewResolver();
+        vr.setPrefix("/WEB-INF/pages/");
+        vr.setSuffix(".jsp");
+        return vr;
+    }
 }
