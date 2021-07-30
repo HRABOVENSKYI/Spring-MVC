f1bc2a375629d7ecd6d093c7315e721c7476c4b4
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-07-30 18:12:54 +0300

    @RequestParam() instead of calling methods

--- src/main/java/org/example/controllers/AddController.java
+++ src/main/java/org/example/controllers/AddController.java
@@ -3,6 +3,7 @@ package org.example.controllers;
 import org.example.services.AddService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,10 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 public class AddController {
 
     @RequestMapping("/add")
-    public ModelAndView add(HttpServletRequest request) {
-
-        int i = Integer.parseInt(request.getParameter("t1"));
-        int j = Integer.parseInt(request.getParameter("t2"));
+    public ModelAndView add(@RequestParam("t1") int i, @RequestParam("t2") int j) {
 
         AddService service = new AddService();
         int k = service.add(i, j);
