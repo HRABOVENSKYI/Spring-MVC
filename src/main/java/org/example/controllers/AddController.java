c24e72ea7f42e7caff2bc2636401fbbd477a5b30
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-07-31 11:56:25 +0300

    Moved index page to the WEB-INF/pages dir

--- src/main/java/org/example/controllers/AddController.java
+++ src/main/java/org/example/controllers/AddController.java
@@ -3,6 +3,7 @@ package org.example.controllers;
 import org.example.services.AddService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,7 +18,12 @@ public class AddController {
         this.service = service;
     }
 
-    @RequestMapping("/add")
+    @GetMapping("/")
+    public String getIndexPage() {
+        return "index";
+    }
+
+    @GetMapping("/add")
     public ModelAndView add(@RequestParam("t1") int i, @RequestParam("t2") int j) {
 
         int k = service.add(i, j);
